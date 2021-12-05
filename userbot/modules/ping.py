1d9d1f91228d008d1a38f5e1f5df01473829d029
oscarprojek <95014364+oscarprojek@users.noreply.github.com>
2021-12-05 17:38:08 +0800

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -73,7 +73,7 @@ async def redis(pong):
     await asyncio.sleep(1)
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**KONTOOLLLL!!**\n**KEKUATAN KONTOL** : `%sms`\n**DURASI KONTOL** : `{uptime}🕛`" % (duration))
+    await pong.edit(f"**MEMEK!!**\n**KEKUATAN MEMEK** : `%sms`\n**DURASI MEMEK** : `{uptime}🕛`" % (duration))
 
 
 @register(outgoing=True, pattern="^Ping$")
